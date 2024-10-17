4f5c76de5a896ba5222dcd8d2f46361319cff560
Demo User <demo@user.com>
2024-10-17 13:25:23 +0530

    "Updated Dashboard"

--- demo_project/dashboards/test.py
+++ demo_project/dashboards/test.py
@@ -5,11 +5,11 @@ class Test(Dashboard):
     __dashboard_name__ = "test"
     __dashboard_description__ = "test desc"
 
-    __grid_root_element__ = {'type': 'branch', 'data': [{'type': 'leaf', 'data': {'views': ['panel-IP1_fhJ_R'], 'activeView': 'panel-IP1_fhJ_R', 'id': '1'}, 'size': 259}, {'type': 'leaf', 'data': {'views': ['panel-_NXT8QrunU'], 'activeView': 'panel-_NXT8QrunU', 'id': '2'}, 'size': 259}, {'type': 'leaf', 'data': {'views': ['panel-SrQ6YWB5ry'], 'activeView': 'panel-SrQ6YWB5ry', 'id': '3'}, 'size': 258.015625}], 'size': 1787.953125}
-    __grid_width__ = 1787.953125
-    __grid_height__ = 776.015625
+    __grid_root_element__ = {'type': 'branch', 'data': [{'type': 'leaf', 'data': {'views': ['metrics-aYDu6rKw0'], 'activeView': 'metrics-aYDu6rKw0', 'id': '1'}, 'size': 183}, {'type': 'leaf', 'data': {'views': ['panel-AFP3xePe-_'], 'activeView': 'panel-AFP3xePe-_', 'id': '2'}, 'size': 183}, {'type': 'leaf', 'data': {'views': ['panel-hqaQcmw5bo'], 'activeView': 'panel-hqaQcmw5bo', 'id': '3'}, 'size': 184.375}], 'size': 1778.296875}
+    __grid_width__ = 1778.296875
+    __grid_height__ = 550.375
     __grid_orientation__ = "VERTICAL"
-    __active_group__ = 3
+    __active_group__ = 1
 
     component_1 = PanelComponent(component_type="metrics",
                                  name="DemoMetrics",
