bd3a71e9043b4b2d280e8c7a91391430cfcf10a1
Demo User <demo@user.com>
2024-10-17 15:26:24 +0530

    "Updated Dashboard"

--- demo_project/dashboards/test.py
+++ demo_project/dashboards/test.py
@@ -5,11 +5,11 @@ class Test(Dashboard):
     __dashboard_name__ = "test"
     __dashboard_description__ = "test"
 
-    __grid_root_element__ = {'type': 'branch', 'data': [{'type': 'leaf', 'data': {'views': ['metrics_kQwa4HqAkZ'], 'activeView': 'metrics_kQwa4HqAkZ', 'id': '1'}, 'size': 178}, {'type': 'leaf', 'data': {'views': ['panel__NXT8QrunU'], 'activeView': 'panel__NXT8QrunU', 'id': '2'}, 'size': 178}, {'type': 'leaf', 'data': {'views': ['panel_SrQ6YWB5ry'], 'activeView': 'panel_SrQ6YWB5ry', 'id': '3'}, 'size': 176.8125}], 'size': 1778.296875}
+    __grid_root_element__ = {'type': 'branch', 'data': [{'type': 'leaf', 'data': {'views': ['metrics_kQwa4HqAkZ'], 'activeView': 'metrics_kQwa4HqAkZ', 'id': '1'}, 'size': 178}, {'type': 'leaf', 'data': {'views': ['tables_dFlPszmSDg'], 'activeView': 'tables_dFlPszmSDg', 'id': '2'}, 'size': 178}, {'type': 'leaf', 'data': {'views': ['panel_SrQ6YWB5ry'], 'activeView': 'panel_SrQ6YWB5ry', 'id': '3'}, 'size': 176.8125}], 'size': 1778.296875}
     __grid_width__ = 1778.296875
     __grid_height__ = 532.8125
     __grid_orientation__ = "VERTICAL"
-    __active_group__ = 1
+    __active_group__ = 2
 
     metrics_kQwa4HqAkZ = PanelComponent(component_type="metrics",
                           name="DemoMetric",
@@ -19,14 +19,14 @@ class Test(Dashboard):
                           description="DemoMetricDesc",
                           columns=[],
                           content_component="mediator")
-    panel__NXT8QrunU = PanelComponent(component_type="metrics",
-                          name="Panel 2",
-                          query="Select(func.count(EmpDept.emp_no).label('Count Employees'))",
-                          data_objects={'EmpDept': 'efficieno.experiment.demo_ontology_layer'},
-                          header="Demo Metric Header",
-                          description="Demo Metric Description",
-                          columns=[],
-                          content_component="default")
+    tables_dFlPszmSDg = PanelComponent(component_type="tables",
+                          name="DemoTable",
+                          query="Select(OeOrderHeadersAll.header_id,OeOrderHeadersAll.order_number,OeOrderHeadersAll.ordered_date)",
+                          data_objects={'OeOrderHeadersAll': 'demo_project.data_objects.ont.oe_order_headers_all'},
+                          header="DemoTable",
+                          description="DemoTableDesc",
+                          columns=[{'accessorKey': 'header_id', 'autoincrement': None, 'dataObject': None, 'dataType': 'Integer', 'databaseTableName': None, 'databaseTableSchema': None, 'dbDataType': 'Integer', 'displayName': 'HeaderID', 'function': 'header_id', 'name': 'header_id', 'nullable': None, 'primaryKey': True, 'unique': None, 'visible': False}, {'accessorKey': 'order_number', 'autoincrement': None, 'dataObject': None, 'dataType': 'Integer', 'databaseTableName': None, 'databaseTableSchema': None, 'dbDataType': 'Integer', 'displayName': None, 'function': 'order_number', 'name': 'order_number', 'nullable': None, 'primaryKey': False, 'unique': None, 'visible': True}, {'accessorKey': 'ordered_date', 'autoincrement': None, 'dataObject': None, 'dataType': 'Date', 'databaseTableName': None, 'databaseTableSchema': None, 'dbDataType': 'Date', 'displayName': None, 'function': 'ordered_date', 'name': 'ordered_date', 'nullable': None, 'primaryKey': False, 'unique': None, 'visible': True}],
+                          content_component="mediator")
     panel_SrQ6YWB5ry = PanelComponent(component_type="metrics",
                           name="Panel 3",
                           query="Select(func.count(EmpDept.emp_no).label('Count Employees'))",
