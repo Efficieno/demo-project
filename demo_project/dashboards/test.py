81c8641cde2079612f11853630e6b9f7479ed464
Demo User <demo@user.com>
2024-10-17 14:40:48 +0530

    "Created file demo_project/dashboards/test.py"

--- demo_project/dashboards/test.py
+++ demo_project/dashboards/test.py
@@ -5,7 +5,7 @@ class Test(Dashboard):
     __dashboard_name__ = "test"
     __dashboard_description__ = "test"
 
-    __grid_root_element__ = {'type': 'branch', 'data': [{'type': 'leaf', 'data': {'views': ['panel-IP1_fhJ_R'], 'activeView': 'panel-IP1_fhJ_R', 'id': '1'}, 'size': 259}, {'type': 'leaf', 'data': {'views': ['panel-_NXT8QrunU'], 'activeView': 'panel-_NXT8QrunU', 'id': '2'}, 'size': 259}, {'type': 'leaf', 'data': {'views': ['panel-SrQ6YWB5ry'], 'activeView': 'panel-SrQ6YWB5ry', 'id': '3'}, 'size': 258.015625}], 'size': 1787.953125}
+    __grid_root_element__ = {'type': 'branch', 'data': [{'type': 'leaf', 'data': {'views': ['panel_IP1_fhJ_R'], 'activeView': 'panel_IP1_fhJ_R', 'id': '1'}, 'size': 259}, {'type': 'leaf', 'data': {'views': ['panel__NXT8QrunU'], 'activeView': 'panel__NXT8QrunU', 'id': '2'}, 'size': 259}, {'type': 'leaf', 'data': {'views': ['panel_SrQ6YWB5ry'], 'activeView': 'panel_SrQ6YWB5ry', 'id': '3'}, 'size': 258.015625}], 'size': 1787.953125}
     __grid_width__ = 1787.953125
     __grid_height__ = 776.015625
     __grid_orientation__ = "VERTICAL"
